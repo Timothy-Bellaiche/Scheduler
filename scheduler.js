86bb2929cc6d11180b6cb3730498fe9bbf4db2ce
Damien <damien@mail.mewfree.com>
2014-12-17 06:26:13 +0100

    initial commit

---
+++ scheduler.js
@@ -0,0 +1,31 @@
+Courses = new Mongo.Collection("courses");
+Courses.initEasySearch('courseName', {
+  'limit' : 10
+});
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    courses: function () {
+      return Courses.find({});
+    },
+    courseschecked: function () {
+      return Courses.find({checked: true});
+    }
+  });
+
+  Template.body.events({
+  });
+
+  Template.coursedesc.events({
+    "click .toggle-checked": function () {
+      Courses._collection.update(this._id, {$set: {checked: ! this.checked}});
+    }
+  })
+
+  Template.coursescheckeddesc.events({
+    "click .toggle-checked": function () {
+      Courses._collection.update(this._id, {$set: {checked: ! this.checked}});
+    }
+  })
+}
