8bf73240fb4c562c37f26840d39297518424cbe4
MewFree <damien.gonot@gmail.com>
2015-01-04 13:13:33 +0800

    updated indexes

--- scheduler.js
+++ scheduler.js
@@ -35,20 +35,20 @@ if (Meteor.isClient) {
 
   Template.schedule.helpers({
     trschedules: [
-      { index: 8, indexp: index+1 },
-      { index: 9 },
-      { index: 10 },
-      { index: 11 },
-      { index: 12 },
-      { index: 13 },
-      { index: 14 },
-      { index: 15 },
-      { index: 16 },
-      { index: 17 },
-      { index: 18 },
-      { index: 19 },
-      { index: 20 },
-      { index: 21 }
+      { index: 8, indexp: 9 },
+      { index: 9, indexp: 10 },
+      { index: 10, indexp: 11},
+      { index: 11, indexp: 12},
+      { index: 12, indexp: 13},
+      { index: 13, indexp: 14},
+      { index: 14, indexp: 15},
+      { index: 15, indexp: 16},
+      { index: 16, indexp: 17},
+      { index: 17, indexp: 18},
+      { index: 18, indexp: 19},
+      { index: 19, indexp: 20},
+      { index: 20, indexp: 21},
+      { index: 21, indexp: 22}
     ]
   });
 
